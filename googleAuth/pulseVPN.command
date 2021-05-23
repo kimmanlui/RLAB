417aeff6be2e22bea38a776e749e9c6a3fe50cd1
kimman <cskmlui@hotmail.com>
2021-05-23 13:20:38 +0800

    pulseVNP code

---
+++ googleAuth/pulseVPN.command
@@ -0,0 +1,1 @@
+python /Users/admin/gitdir/googleAuth/pulseVPN.py
---
+++ googleAuth/pulseVPN.py
@@ -0,0 +1,17 @@
+#!pip install pyotp
+import pyotp
+
+from bs4 import BeautifulSoup
+infile = open("/Users/admin/Documents/Dropbox/jupyter/prj/logininfo.xml","r")
+contents = infile.read()
+soup = BeautifulSoup(contents,'xml')
+
+token  =soup.conf.token.get_text()
+totp = pyotp.TOTP(token)
+code=totp.now()
+from time import gmtime, strftime
+import pyperclip
+pyperclip.copy(code)
+print(strftime("%S", gmtime())+"   "+code)
+import time
+time.sleep(15)
