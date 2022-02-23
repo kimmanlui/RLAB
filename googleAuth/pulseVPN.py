049e7359ca8f76294f3e4bd18c853c03c8d52386
macbook <macbook@abc.com>
2022-02-23 18:02:35 +0800

    datastore

--- googleAuth/pulseVPN.py
+++ googleAuth/pulseVPN.py
@@ -4,7 +4,7 @@ import pyotp
 from bs4 import BeautifulSoup
 infile = open("/Users/admin/Documents/Dropbox/jupyter/prj/logininfo.xml","r")
 contents = infile.read()
-soup = BeautifulSoup(contents,'xml')
+soup = BeautifulSoup(contents,"html5lib")
 
 token  =soup.conf.token.get_text()
 totp = pyotp.TOTP(token)
