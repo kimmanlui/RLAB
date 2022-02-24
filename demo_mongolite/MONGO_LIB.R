d1b6b676ab06aedf612c308e0bf29474cbe74a2b
kimman123 <kimman.km.lui@pccw.com>
2022-02-24 18:49:46 +0800

    mongo

--- demo_mongolite/MONGO_LIB.R
+++ demo_mongolite/MONGO_LIB.R
@@ -47,12 +47,28 @@ updateMongoDF=function(where.df, update.df, mg)
   return(retV)
 }
 
+updateMongoDF=function(where.df, update.df, mg)
+{
+  # Eg. updateMongoDF( df[1,'COL1', drop=FALSE], df[1,c('COL2','COL3'), drop=FALSE], mg)
+  wst=toJSON(where.df[1,,drop=FALSE])
+  jst=toJSON(update.df[1,,drop=FALSE])
+  monQ=substr(wst,2,(nchar(wst)-1))
+  monQ=paste0("{",monQ,"}")
+  #print(monQ)
+  monU=substr(jst,2,(nchar(jst)-1))
+  monU=paste0("{",monU,"}")
+  monU=paste0( '{"$set":  ' , monU , "}")
+  #print(monU)
+  retV=mg$update(query=monQ,  update=monU )
+  return(retV)
+}
 
 findMongoDF=function(where.df, mg)
 {
   # Eg. updateMongoDF( df[1,'COL1', drop=FALSE], df[1,c('COL2','COL3'), drop=FALSE], mg)
   wst=toJSON(where.df[1,,drop=FALSE])
   monQ=substr(wst,2,(nchar(wst)-1))
+  monQ=paste0("{",monQ,"}")
   #print(monQ)
   monU=paste0( '{"$set":  ' , substr(jst,2,(nchar(jst)-1)) , "}")
   #print(monU)
