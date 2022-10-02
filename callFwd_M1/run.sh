b49867cffd6cafe2e2bd7b988c08d3a2e884c0a5
m1 <learn2022@hotmail.com>
2022-10-02 23:00:04 +0800

    datastore

--- callFwd_M1/run.sh
+++ callFwd_M1/run.sh
@@ -1,31 +1,23 @@
+#!/bin/zsh
 
-
-echo off
-
-export MF=/Users/admin/Library/CloudStorage/Box-Box
+echo on
+export MF=/Users/admin/Library/CloudStorage/Box-Box/M1_ROOT
 
 #SET MF="c:\users\cskml\box"
 
+export MASTERFILE=$MF/confFwd.txt 
 
+export BASE=/Users/admin/RLAB/callFwd_M1
+export SRC=$BASE
+export JAR=$BASE
+export CP=$JAR/jTCPfwd.jar:$SRC
+export JAVA_HOME=/usr
+export RERUN=112 
 
-set BASE=/kim/gitdir/RLAB/callFwd
-set SRC=%BASE%
-set JAR=%BASE%
-set CP="%JAR%/jTCPfwd.jar;%SRC%"
-REM set JAVA_HOME=/progra~1/graalvm-ce-java8-19.3.1
-set JAVA_HOME=/progra~1/JAVA/jdk1.8.0_301
-REM %JAVA_HOME%/bin/javac  -cp  %CP% %SRC%/CallFwd.java
-REM Unit Test
-set RERUN=112 
+cd $MF
+echo $JAVA_HOME/bin/java
+java -version
+java -cp $CP  CallFwd $MASTERFILE
+STATUS="${?}"
+echo "${STATUS}"
 
-set RERUN=%ERRORLEVEL%
-:loop
-echo %RERUN%
-IF %RERUN% EQU 112 GOTO ERROR
-%JAVA_HOME%/bin/java -cp %CP%  CallFwd %MASTERFILE%
-set RERUN=%ERRORLEVEL%
-goto loop
-
-
-:ERROR
-ECHO "Program END"
