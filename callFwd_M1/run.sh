1085ddeb82b782a75927b5f2d408d6470557c14d
m1 <learn2022@hotmail.com>
2022-10-03 09:27:51 +0800

    datastore

--- callFwd_M1/run.sh
+++ callFwd_M1/run.sh
@@ -5,7 +5,7 @@ export MF=/Users/admin/Library/CloudStorage/Box-Box/M1_ROOT
 
 #SET MF="c:\users\cskml\box"
 
-export MASTERFILE=$MF/confFwd.txt 
+export MASTERFILE=/Users/admin/RLAB/callFwd_M1/confMaster.txt 
 
 export BASE=/Users/admin/RLAB/callFwd_M1
 export SRC=$BASE
@@ -17,7 +17,12 @@ export RERUN=112
 cd $MF
 echo $JAVA_HOME/bin/java
 java -version
-java -cp $CP  CallFwd $MASTERFILE
-STATUS="${?}"
-echo "${STATUS}"
+STATUS="111"
+while [ $STATUS -lt 112 ]
+do
+  java -cp $CP  CallFwd $MASTERFILE
+  STATUS="${?}"
+  echo "${STATUS}"
+done
 
+echo "---END---"
